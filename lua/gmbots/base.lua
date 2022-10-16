e68a1e21f4f57fa9068a4678adb541ced4370b48
nubz4lif <thenoobattf2@gmail.com>
2022-10-16 15:26:45 -0400

    Added more to base.lua

--- lua/gmbots/base.lua
+++ lua/gmbots/base.lua
@@ -2,7 +2,7 @@
 	This file can act as a base for custom gamemode support.
 	You can read the wiki at:
 	https://github.com/Noobz4Life/GMBots/wiki
-	
+
 	Note that this wiki may not be up to date, as the code had been completely rewritten.
 ]]
 
@@ -13,7 +13,7 @@ end)
 hook.Add("GMBotsStart","GMBots_RunStart",function(ply,cmd) -- Initialize the hook.
 	cmd:ClearButtons() -- Clear any buttons the bot is pressing, this is usually crouch.
 	cmd:ClearMovement() -- Clear any movement the bot is doing, this usually doesn't do anything but it's here just in case.
-	
+
 	if ply.Enemy and ply.Enemy:IsValid() and ply.Enemy:IsPlayer() and ply.Enemy:Alive() then -- Check if we have a enemy, and that the enemy is a player and is still alive..
 		-- PLAYER:AttackPlayer(enemy, closest_distance, maximum_distance, hold_down_attack)
 		ply:AttackPlayer(ply.Enemy,200,800,true) -- Attack the enemy, this will also pathfind for you if the enemy is out of sight.
@@ -21,4 +21,15 @@ hook.Add("GMBotsStart","GMBots_RunStart",function(ply,cmd) -- Initialize the hoo
 		ply.Enemy = ply:LookForPlayers() or nil -- Look for a new enemy.
 		ply:Wander() -- Wander around while waiting for a new enemy.
 	end
+end)
+
+hook.Add("GMBotsTakeDamage","GMBots_TakeDamage",function(ply,dmg)
+	local attacker = dmg:GetAttacker()
+	if(attacker and attacker:IsPlayer() and attacker ~= ply) then
+		ply.Enemy = attacker
+	end
+end)
+
+hook.Add("GMBotsDeath","GMBots_BotDeath",function(ply,inflictor,attacker)
+	ply.Enemy = nil
 end)
